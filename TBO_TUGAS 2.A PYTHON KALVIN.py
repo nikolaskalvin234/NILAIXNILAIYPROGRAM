2eaa1dcebcef6cf9eff1104efa9f4dcecccf38c1
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 18:47:38 +0700

    first commit

---
+++ TBO_TUGAS 2.A PYTHON KALVIN.py
@@ -0,0 +1,21 @@
+#.CODING PYTHON - [ TUGAS - 2.A ]
+#.KULIAH TEORI BAHASA & OTOMATA
+#.Python Matematika = Menukar variabel
+#.DOSEN = Arnes Y.V, M.Kom
+
+print("NAMA = NIKOLAS KALVIN")
+print("NPM = 21421002")
+print('')
+
+# Menginput Nilai Variabel 
+x = input('Tuliskan nilai x: ')
+y = input('Tuliskan nilai y: ')
+
+# Membuat Variabel tukar dan Menukar nilai Variabel lain 
+tukar = x
+x = y
+y = tukar
+
+#Menampilkan Nilai Variabel Setelah Ditukar
+print('Nilai x Setelah Ditukar adalah: {}'.format(x))
+print('Nilai y Setelah Ditukar adalah: {}'.format(y))
